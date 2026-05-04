ad293cb1634c521a1f562858b18ebb9d07dad4e9
lucy <email>
2026-05-04 06:29:43 +0000

    commit

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,4 +1,5 @@
 from .q_learner import QLearner
+from .dmaq_qatten_learner import DMAQ_qattenLearner
 from .q_learner_DIVA import QLearnerDIVA
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
@@ -8,6 +9,7 @@ from .casec_learner import CASECLearner
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
+REGISTRY["dmaq_qatten_learner"] = DMAQ_qattenLearner
 REGISTRY["q_learner_DIVA"] = QLearnerDIVA
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["qtran_learner"] = QTranLearner
